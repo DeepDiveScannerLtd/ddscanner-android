2720298819bea99d61592e84e581b7cc86011678
lashket <lashket@gmail.com>
2016-02-01 20:15:42 +0300

    Get user info

--- trunk/android-app/app/src/main/java/travel/ilave/deepdivescanner/ui/activities/SocialLogin.java
+++ trunk/android-app/app/src/main/java/travel/ilave/deepdivescanner/ui/activities/SocialLogin.java
@@ -1,14 +1,23 @@
 package travel.ilave.deepdivescanner.ui.activities;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 import travel.ilave.deepdivescanner.R;
 
@@ -29,11 +38,23 @@ public class SocialLogin extends AppCompatActivity {
         setContentView(R.layout.activity_social_login);
 
         loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
-
+        loginButton.setReadPermissions(Arrays.asList("public_profile, email, user_birthday, user_friends"));
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
+                Profile profile = Profile.getCurrentProfile();
+                profile.getName();
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+                                Log.v("LoginActivity", response.toString());
+                            }
+                        });
             }
 
             @Override
