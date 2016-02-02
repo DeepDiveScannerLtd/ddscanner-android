dbd1be6f61ac26076a1e658e23b6a5a2926de93a
lashket <lashket@gmail.com>
2016-02-02 18:57:30 +0300

    Google sign in

--- trunk/android-app/app/src/main/java/travel/ilave/deepdivescanner/ui/activities/SocialLogin.java
+++ trunk/android-app/app/src/main/java/travel/ilave/deepdivescanner/ui/activities/SocialLogin.java
@@ -1,15 +1,12 @@
 package travel.ilave.deepdivescanner.ui.activities;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.service.textservice.SpellCheckerService;
+import android.content.IntentSender;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -22,9 +19,13 @@ import com.facebook.HttpMethod;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.facebook.login.widget.ProfilePictureView;
-import com.facebook.share.model.ShareLinkContent;
-import com.facebook.share.widget.ShareDialog;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.model.LatLng;
 
 import org.json.JSONException;
@@ -32,9 +33,14 @@ import org.json.JSONObject;
 
 import travel.ilave.deepdivescanner.R;
 
-public class SocialLogin extends AppCompatActivity {
-    CallbackManager callbackManager;
-    LoginButton login;
+public class SocialLogin extends AppCompatActivity implements View.OnClickListener{
+
+    /* For google plus */
+    private GoogleApiClient mGoogleApiClient;
+    private SignInButton signInButton;
+    /* For facebook */
+    private CallbackManager callbackManager;
+    private LoginButton login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +48,25 @@ public class SocialLogin extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_social_login);
 
+        /*Google*/
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        signInButton = (SignInButton) findViewById(R.id.google_signin_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setScopes(gso.getScopeArray());
+        signInButton.setOnClickListener(this);
+
+        /*Facebook*/
         callbackManager = CallbackManager.Factory.create();
         login = (LoginButton) findViewById(R.id.facebook_login_button);
         login.setReadPermissions("public_profile email");
@@ -76,7 +101,6 @@ public class SocialLogin extends AppCompatActivity {
             public void onError(FacebookException exception) {
             }
         });
-
     }
 
     public void RequestData() {
@@ -101,7 +125,14 @@ public class SocialLogin extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        System.out.println("Request code is - - - " + resultCode + " - " + requestCode + " - " + data);
+        if (requestCode == 1) {
+            System.out.println("1111");
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 
@@ -121,6 +152,35 @@ public class SocialLogin extends AppCompatActivity {
             }
         }).executeAsync();
     }
+
+    /*Google plus*/
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.google_signin_button:
+              //  if (GoogleApiClient.on)
+                signIn();
+                break;
+            // ...
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, 1);
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("LOGIN", "handleSignInResult:" + result.isSuccess() + result.getStatus());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            System.out.println(acct.getEmail());
+        } else {
+            // Signed out, show unauthenticated UI.
+        }
+    }
+
     public static void show(Context context, LatLng latLng) {
         Intent intent = new Intent(context, SocialLogin.class);
         intent.putExtra("LATLNG", latLng);
