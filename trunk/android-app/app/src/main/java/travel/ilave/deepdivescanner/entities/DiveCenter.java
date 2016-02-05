bab81eabe6eeacb1b82e95d8096f02332faf9563
lashket <lashket@gmail.com>
2016-02-05 10:41:04 +0300

    DiveCenters entity

---
+++ trunk/android-app/app/src/main/java/travel/ilave/deepdivescanner/entities/DiveCenter.java
@@ -0,0 +1,74 @@
+package travel.ilave.deepdivescanner.entities;
+
+import java.io.Serializable;
+
+/**
+ * Created by lashket on 5.2.16.
+ */
+public class DiveCenter implements Serializable {
+
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+    private String lat;
+    private String lng;
+    private String logo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+}
