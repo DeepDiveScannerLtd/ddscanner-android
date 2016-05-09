a2a089cbb56d30b0755d8df0ccd2ae4d4a6add31
Lashkevich Andrei <lashket@gmail.com>
2016-05-09 15:41:16 +0300

    Profile edit layout

--- trunk/ddscanner/app/src/main/java/com/ddscanner/ui/fragments/ProfileFragment.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/ui/fragments/ProfileFragment.java
@@ -5,13 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.ddscanner.R;
 
 /**
  * Created by lashket on 20.4.16.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
+
+    private LinearLayout editProfile;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,7 +25,18 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        editProfile = (LinearLayout) v.findViewById(R.id.edit_profile);
+
+
+        return v;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.edit_profile:
+                break;
+        }
+    }
 }
