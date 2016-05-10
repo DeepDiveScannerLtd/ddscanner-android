b3cf509bd73b0fa535486238b2213df55f8eaba6
Lashkevich Andrei <lashket@gmail.com>
2016-05-10 13:10:58 +0300

    New comment entity

--- trunk/ddscanner/app/src/main/java/com/ddscanner/entities/Comment.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/entities/Comment.java
@@ -1,6 +1,7 @@
 package com.ddscanner.entities;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by lashket on 12.3.16.
@@ -10,6 +11,43 @@ public class Comment implements Serializable {
     private User user;
     private String rating;
     private String id;
+    private List<String> images;
+    private String date;
+    private String likes;
+    private String dislikes;
+    private User user;
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+
+    public String getDislikes() {
+        return dislikes;
+    }
+
+    public void setDislikes(String dislikes) {
+        this.dislikes = dislikes;
+    }
 
     public String getId() {
         return id;
