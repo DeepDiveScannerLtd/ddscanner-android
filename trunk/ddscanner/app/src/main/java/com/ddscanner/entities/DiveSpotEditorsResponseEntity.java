4182b5e4a0732788bf6274b492a70228d545a07e
Vitaly Duong <duongvitaly@gmail.com>
2016-05-27 20:26:03 +0300

    + dive spot editors (not finished)

---
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/entities/DiveSpotEditorsResponseEntity.java
@@ -0,0 +1,16 @@
+package com.ddscanner.entities;
+
+import java.util.List;
+
+public class DiveSpotEditorsResponseEntity {
+
+    private List<User> editors;
+
+    public List<User> getEditors() {
+        return editors;
+    }
+
+    public void setEditors(List<User> editors) {
+        this.editors = editors;
+    }
+}
