bd627debdc820e42fb053d3b74b84206a90f1a2b
Lashkevich Andrei <lashket@gmail.com>
2016-07-22 01:08:18 +0300

    Image and author entities

---
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/entities/Author.java
@@ -0,0 +1,44 @@
+package com.ddscanner.entities;
+
+/**
+ * Created by lashket on 22.7.16.
+ */
+public class Author {
+
+    private String type;
+    private String name;
+    private String photo;
+    private String date;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/entities/Image.java
@@ -0,0 +1,27 @@
+package com.ddscanner.entities;
+
+
+/**
+ * Created by lashket on 22.7.16.
+ */
+public class Image {
+
+    private String name;
+    private Author author;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
