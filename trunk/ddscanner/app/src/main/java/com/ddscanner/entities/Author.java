f13cd8cfa13d55af9833a2466877a535dbac316b
Lashkevich Andrei <lashket@gmail.com>
2016-07-29 16:15:25 +0300

    Creating parcelable intities Image and Author

--- trunk/ddscanner/app/src/main/java/com/ddscanner/entities/Author.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/entities/Author.java
@@ -1,14 +1,49 @@
 package com.ddscanner.entities;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by lashket on 22.7.16.
  */
-public class Author {
+public class Author implements Parcelable {
 
     private String type;
     private String name;
     private String photo;
     private String date;
+    
+    protected Author(Parcel in) {
+        type = in.readString();
+        name = in.readString();
+        photo = in.readString();
+        date = in.readString();
+    }
+
+    public static final Creator<Author> CREATOR = new Creator<Author>() {
+        @Override
+        public Author createFromParcel(android.os.Parcel in) {
+            return new Author(in);
+        }
+
+        @Override
+        public Author[] newArray(int size) {
+            return new Author[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(type);
+        parcel.writeString(name);
+        parcel.writeString(photo);
+        parcel.writeString(date);
+    }
 
     public String getType() {
         return type;
--- trunk/ddscanner/app/src/main/java/com/ddscanner/entities/Image.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/entities/Image.java
@@ -1,13 +1,44 @@
 package com.ddscanner.entities;
 
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by lashket on 22.7.16.
  */
-public class Image {
+public class Image implements Parcelable {
 
     private String name;
     private Author author;
+
+    protected Image(Parcel in) {
+        name = in.readString();
+        author = in.readParcelable(Author.class.getClassLoader());
+    }
+
+    public static final Creator<Image> CREATOR = new Creator<Image>() {
+        @Override
+        public Image createFromParcel(android.os.Parcel in) {
+            return new Image(in);
+        }
+
+        @Override
+        public Image[] newArray(int size) {
+            return new Image[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeParcelable(author, i);
+    }
 
     public String getName() {
         return name;
