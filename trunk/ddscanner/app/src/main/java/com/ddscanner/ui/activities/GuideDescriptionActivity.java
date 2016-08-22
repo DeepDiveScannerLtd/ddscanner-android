024557ba141f9f6f16eeddd1deba3faca3278879
lashket <lashket@gmail.com>
2016-08-22 17:35:10 +0300

    * guide thanks layout

--- trunk/ddscanner/app/src/main/java/com/ddscanner/ui/activities/GuideDescriptionActivity.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/ui/activities/GuideDescriptionActivity.java
@@ -1,12 +1,15 @@
 package com.ddscanner.ui.activities;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ddscanner.R;
@@ -16,12 +19,15 @@ import com.ddscanner.utils.Constants;
 /**
  * Created by lashket on 5.8.16.
  */
-public class GuideDescriptionActivity extends AppCompatActivity {
+public class GuideDescriptionActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView title;
     private TextView description;
+    private TextView thnaks;
     private Toolbar toolbar;
     private GuideItem item;
+    private ImageView buttonYes;
+    private ImageView buttonNo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,9 @@ public class GuideDescriptionActivity extends AppCompatActivity {
         title = (TextView) findViewById(R.id.title);
         description = (TextView) findViewById(R.id.description);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        buttonNo = (ImageView) findViewById(R.id.no);
+        buttonYes = (ImageView) findViewById(R.id.yes);
+        thnaks = (TextView) findViewById(R.id.titleUseful);
 
         item = (GuideItem) getIntent().getSerializableExtra(Constants.GUIDE_DESCRIPTION_ACTIVITY_INTENT_ITEM);
 
@@ -41,6 +50,8 @@ public class GuideDescriptionActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_ac_back);
         getSupportActionBar().setTitle(R.string.guide_to_dds);
+        buttonNo.setOnClickListener(this);
+        buttonYes.setOnClickListener(this);
     }
 
     @Override
@@ -56,5 +67,23 @@ public class GuideDescriptionActivity extends AppCompatActivity {
         Intent intent = new Intent(context, GuideDescriptionActivity.class);
         intent.putExtra(Constants.GUIDE_DESCRIPTION_ACTIVITY_INTENT_ITEM, guideItem);
         context.startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.yes:
+                buttonYes.setBackground(getResources().getDrawable(R.drawable.ic_smile_active));
+                thnaks.setText(R.string.thank_you_title);
+                buttonYes.setOnClickListener(null);
+                buttonNo.setOnClickListener(null);
+                break;
+            case R.id.no:
+                buttonNo.setBackground(getResources().getDrawable(R.drawable.ic_sad_active));
+                thnaks.setText(R.string.thank_you_title);
+                buttonYes.setOnClickListener(null);
+                buttonNo.setOnClickListener(null);
+                break;
+        }
     }
 }
