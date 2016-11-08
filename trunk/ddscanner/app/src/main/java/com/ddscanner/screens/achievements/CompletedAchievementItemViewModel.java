3e223dfd3ffcf282b91a7c5d4faaa599262a8594
lashket <lashket@gmail.com>
2016-11-08 18:13:24 +0300

    ~ complete achievemtn using data binding

--- trunk/ddscanner/app/src/main/java/com/ddscanner/screens/achievements/CompletedAchievementItemViewModel.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/screens/achievements/CompletedAchievementItemViewModel.java
@@ -1,6 +1,14 @@
 package com.ddscanner.screens.achievements;
 
+import android.databinding.BindingAdapter;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
 import com.ddscanner.entities.CompleteAchievement;
+import com.ddscanner.ui.views.AchievementCountryFlagView;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class CompletedAchievementItemViewModel {
 
@@ -12,6 +20,28 @@ public class CompletedAchievementItemViewModel {
 
     public CompleteAchievement getCompleteAchievement() {
         return completeAchievement;
+    }
+
+    @BindingAdapter("loadCountryFlagFrom")
+    public static void loadCountryFlag(final AchievementCountryFlagView view, CompletedAchievementItemViewModel viewModel) {
+        Target target = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                view.setFlagBitmap(bitmap);
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+
+        Picasso.with(view.getContext()).load(viewModel.getCompleteAchievement().getCountry()).into(target);
     }
 
 }
