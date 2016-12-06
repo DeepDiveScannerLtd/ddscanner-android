866fe1bedbe5e7fcad13a4befc9ca06252f1b707
lashket <lashket@gmail.com>
2016-12-06 16:58:24 +0300

    ~ refactor pick image in add dive spot activity

--- trunk/ddscanner/app/src/main/java/com/ddscanner/screens/divespot/add/AddDiveSpotActivity.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/screens/divespot/add/AddDiveSpotActivity.java
@@ -295,63 +295,12 @@ public class AddDiveSpotActivity extends AppCompatActivity implements View.OnCli
                 }
                 break;
             case ActivitiesRequestCodes.REQUEST_CODE_ADD_DIVE_SPOT_ACTIVITY_PICK_PHOTO:
-                Uri uri = Uri.parse("");
                 if (resultCode == RESULT_OK) {
-                    if (data.getClipData() != null) {
-                        for (int i = 0; i < data.getClipData().getItemCount(); i++) {
-                            String filename = "DDScanner" + String.valueOf(System.currentTimeMillis());
-                            try {
-                                uri = data.getClipData().getItemAt(i).getUri();
-                                String mimeType = getContentResolver().getType(uri);
-                                String sourcePath = getExternalFilesDir(null).toString();
-                                File file = new File(sourcePath + "/" + filename);
-                                if (Helpers.isFileImage(uri.getPath()) || mimeType.contains("image")) {
-                                    try {
-                                        Helpers.copyFileStream(file, uri, this);
-                                        Log.i(TAG, file.toString());
-                                    } catch (Exception e) {
-                                        e.printStackTrace();
-                                    }
-                                    if (photos_rc.getVisibility() == View.VISIBLE) {
-                                        photoUris.add(file.getPath());
-                                    } else {
-                                        mapsUris.add(file.getPath());
-                                    }
-                                } else {
-                                    Toast.makeText(this, "You can choose only images", Toast.LENGTH_SHORT).show();
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
+                    if (photos_rc.getVisibility() == View.VISIBLE) {
+                        photoUris.addAll(Helpers.getPhotosFromIntent(data, this));
+                    } else {
+                        mapsUris.addAll(Helpers.getPhotosFromIntent(data, this));
                     }
-                    if (data.getData() != null) {
-                        String filename = "DDScanner" + String.valueOf(System.currentTimeMillis());
-                        try {
-                            uri = data.getData();
-                            String mimeType = getContentResolver().getType(uri);
-                            String sourcePath = getExternalFilesDir(null).toString();
-                            File file = new File(sourcePath + "/" + filename);
-                            if (Helpers.isFileImage(uri.getPath()) || mimeType.contains("image")) {
-                                try {
-                                    Helpers.copyFileStream(file, uri, this);
-                                    Log.i(TAG, file.toString());
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                                if (photos_rc.getVisibility() == View.VISIBLE) {
-                                    photoUris.add(file.getPath());
-                                } else {
-                                    mapsUris.add(file.getPath());
-                                }
-                            } else {
-                                Toast.makeText(this, "You can choose only images", Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-
                     if (photos_rc.getVisibility() == View.VISIBLE) {
                         photos_rc.setAdapter(new AddPhotoToDsListAdapter(photoUris, AddDiveSpotActivity.this));
                         photos_rc.scrollToPosition(photoUris.size());
