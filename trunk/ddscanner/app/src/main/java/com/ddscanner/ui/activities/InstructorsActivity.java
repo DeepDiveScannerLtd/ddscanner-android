07a2c7b9f72af23a51ce74612e9985f35dec9f37
lashket <lashket@gmail.com>
2017-01-05 16:35:32 +0300

    + instructors activity class

---
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/ui/activities/InstructorsActivity.java
@@ -0,0 +1,13 @@
+package com.ddscanner.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class InstructorsActivity extends BaseAppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
