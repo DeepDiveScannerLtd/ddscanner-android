39dba2bf8e6f129be5bbd74ecb5ef7ec2d26d9fa
lashket <lashket@gmail.com>
2017-01-29 16:53:38 +0300

    (WIP) likes activity

---
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/screens/user/likes/LikePhotoItemViewModel.java
@@ -0,0 +1,11 @@
+package com.ddscanner.screens.user.likes;
+
+import com.ddscanner.entities.LikeEntity;
+
+public class LikePhotoItemViewModel extends LikeReviewViewModel {
+
+    public LikePhotoItemViewModel(LikeEntity likeEntity) {
+        super(likeEntity);
+    }
+
+}
