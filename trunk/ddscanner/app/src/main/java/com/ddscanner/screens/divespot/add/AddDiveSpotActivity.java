027eb923049d44ec0313d01883e73e95e050ca46
lashket <lashket@gmail.com>
2017-02-14 18:14:29 +0300

    + checking if translation also in lst

--- trunk/ddscanner/app/src/main/java/com/ddscanner/screens/divespot/add/AddDiveSpotActivity.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/screens/divespot/add/AddDiveSpotActivity.java
@@ -360,6 +360,12 @@ public class AddDiveSpotActivity extends AppCompatActivity implements CompoundBu
             case ActivitiesRequestCodes.REQUEST_CODE_ADD_DIVE_SPOT_ACTIVITY_PICK_LANGUAGE:
                 if (resultCode == RESULT_OK) {
                     Language language = (Language) data.getSerializableExtra("language");
+                    for (Translation temp : translationsListAdapter.getTranslations()) {
+                        if (temp.getCode().equals(language.getCode())) {
+                            //TODO pogovorit' s sanei po povody daloga
+                            return;
+                        }
+                    }
                     AddTranslationDialogFragment.show(getSupportFragmentManager(), language.getCode(), language.getName(), "", "");
                 }
                 break;
