002f44278da353592624259aea24a803fd840f20
lashket <lashket@gmail.com>
2017-04-14 13:25:50 +0300

    * sealife photos showing when user search sealife

--- trunk/ddscanner/app/src/main/java/com/ddscanner/ui/adapters/SealifeSearchAdapter.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/ui/adapters/SealifeSearchAdapter.java
@@ -127,7 +127,7 @@ public class SealifeSearchAdapter extends RecyclerView.Adapter<SealifeSearchAdap
 
         public void bind(SealifeShort model) {
             name.setText(model.getName());
-            Picasso.with(context).load(DDScannerApplication.getInstance().getString(R.string.base_photo_url, model.getImage(), "2")).resize(photo.getWidth(), Math.round(Helpers.convertDpToPixel(100, context))).transform(new RoundedCornersTransformation(Math.round(Helpers.convertDpToPixel(2, context )), 0, RoundedCornersTransformation.CornerType.TOP)).centerCrop().into(photo);
+            Picasso.with(context).load(DDScannerApplication.getInstance().getString(R.string.base_photo_url, model.getImage(), "2")).transform(new RoundedCornersTransformation(Math.round(Helpers.convertDpToPixel(2, context )), 0, RoundedCornersTransformation.CornerType.TOP)).into(photo);
         }
 
         @Override
