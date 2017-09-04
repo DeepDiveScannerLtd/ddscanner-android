442fabdbd467088bdddc1ded59ecd2165ce163d5
lashket <lashket@gmail.com>
2017-09-04 19:50:18 +0300

    + vide as backgrond in login activity

--- trunk/ddscanner/app/src/main/java/com/ddscanner/ui/activities/LoginActivity.java
+++ trunk/ddscanner/app/src/main/java/com/ddscanner/ui/activities/LoginActivity.java
@@ -1,13 +1,20 @@
 package com.ddscanner.ui.activities;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import com.ddscanner.DDScannerApplication;
 import com.ddscanner.R;
@@ -16,6 +23,8 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 public class LoginActivity extends BaseAppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    VideoView videoView;
 
     public static void show(Context context) {
         Intent intent = new Intent(context, LoginActivity.class);
@@ -26,18 +35,34 @@ public class LoginActivity extends BaseAppCompatActivity implements GoogleApiCli
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_to_continue);
+//        themeNavAndStatusBar();
         findViews();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void themeNavAndStatusBar() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return;
+        Window w = getWindow();
+        w.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//        w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        w.setNavigationBarColor(ContextCompat.getColor(this ,android.R.color.transparent));
+        w.setStatusBarColor(ContextCompat.getColor(this, android.R.color.transparent));
     }
 
     private void findViews() {
         Button signUp = findViewById(R.id.sign_up);
         Button login = findViewById(R.id.login);
-
+        videoView = findViewById(R.id.video_view);
+        Uri uri = Uri.parse(getString(R.string.vido_resource_pattern, getPackageName(), R.raw.login_video));
+        videoView.setVideoURI(uri);
+        videoView.start();
+        videoView.setOnPreparedListener(mediaPlayer -> mediaPlayer.setLooping(true));
         signUp.setOnClickListener(this);
         login.setOnClickListener(this);
 
-        setupToolbar(R.string.login_high, R.id.toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+//        setupToolbar(R.string.login_high, R.id.toolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
     }
 
     @Override
@@ -71,13 +96,13 @@ public class LoginActivity extends BaseAppCompatActivity implements GoogleApiCli
     @Override
     protected void onPause() {
         super.onPause();
-        DDScannerApplication.activityPaused();
+        videoView.pause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        DDScannerApplication.activityResumed();
+        videoView.start();
     }
 
     public static void showForResult(Activity context, int code) {
